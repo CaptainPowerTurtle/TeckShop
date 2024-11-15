bcf0371a31fb830bb8a18fd4760ed3cef0345d16
CptPowerTurtle <powerturtle@protonmail.ch>
2024-11-15 01:42:25 +0100

    fix: removed logger

--- src/services/catalog/Catalog.Application/Features/Brands/GetBrand/GetBrand.cs
+++ src/services/catalog/Catalog.Application/Features/Brands/GetBrand/GetBrand.cs
@@ -4,7 +4,6 @@ using Catalog.Domain.Common.Errors;
 using Catalog.Domain.Entities.Brands;
 using ErrorOr;
 using MapsterMapper;
-using Microsoft.Extensions.Logging;
 using TeckShop.Core.CQRS;
 
 namespace Catalog.Application.Features.Brands.GetBrand
@@ -22,15 +21,12 @@ namespace Catalog.Application.Features.Brands.GetBrand
     /// </remarks>
     /// <param name="mapper">The mapper.</param>
     /// <param name="cache">The cache.</param>
-    /// <param name="logger"></param>
-    internal sealed class GetBrandQueryHandler(IMapper mapper, IBrandCache cache, ILogger<GetBrandQueryHandler> logger) : IQueryHandler<GetBrandQuery, ErrorOr<BrandResponse>>
+    internal sealed class GetBrandQueryHandler(IMapper mapper, IBrandCache cache) : IQueryHandler<GetBrandQuery, ErrorOr<BrandResponse>>
     {
         /// <summary>
         /// The cache.
         /// </summary>
         private readonly IBrandCache _cache = cache;
-
-        private readonly ILogger<GetBrandQueryHandler> _logger = logger;
 
         /// <summary>
         /// The mapper.
@@ -45,7 +41,6 @@ namespace Catalog.Application.Features.Brands.GetBrand
         /// <returns><![CDATA[Task<ErrorOr<BrandResponse>>]]></returns>
         public async Task<ErrorOr<BrandResponse>> Handle(GetBrandQuery request, CancellationToken cancellationToken)
         {
-            _logger.LogInformation("This is a test log for aspire!");
             Brand? brand = await _cache.GetOrSetByIdAsync(request.Id, cancellationToken: cancellationToken);
 
             return brand == null ? (ErrorOr<BrandResponse>)Errors.Brand.NotFound : (ErrorOr<BrandResponse>)_mapper.Map<BrandResponse>(brand);
